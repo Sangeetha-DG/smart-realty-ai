bf8b4eedd2310944b3ad994515c2fe3cadb043c0
Sangeetha-DG <139610334+Sangeetha-DG@users.noreply.github.com>
2025-08-08 15:17:00 -0400

    Delete smart-realty-ai directory

--- smart-realty-ai/realty/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import HouseImage
-
-class HouseImageForm(forms.ModelForm):
-    class Meta:
-        model = HouseImage
-        fields = ['image']
--- smart-realty-ai/realty/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from django.db import models
-
-class HouseImage(models.Model):
-    image = models.ImageField(upload_to='house_images/')
-    description = models.TextField(blank=True)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
